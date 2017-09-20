fe98185e82dfb9abf8ea8322985f803e969c0604
Shinto Joseph <shintojoseph@Shintos-MacBook-Air.local>
2017-09-20 21:11:18 +0530

    Initial Commit

---
+++ My pokidex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My pokidex
+//
+//  Created by Shinto Joseph on 20/09/17.
+//  Copyright © 2017 Shinto Joseph. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
